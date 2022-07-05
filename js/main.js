c12248b328376bb6ea1fb8e14d27f8054f534b8a
tamimhasan19702 <tamimhasancu@gmail.com>
2022-07-06 01:50:28 +0600

    link color upto scroll menu

--- js/main.js
+++ js/main.js
@@ -24,3 +24,23 @@ function linkAction(){
     navmenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click' , linkAction))
+
+// =============scroll section active========================================
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageXOffset
+
+  sections.forEach( current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+    }else{
+        document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+    }
+  })
+}
