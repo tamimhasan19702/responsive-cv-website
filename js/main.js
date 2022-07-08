36dd1b0373167286b5d54336396d99113c6a6a0b
tamimhasan19702 <tamimhasancu@gmail.com>
2022-07-09 01:52:58 +0600

    scroll top add + responsive done

--- js/main.js
+++ js/main.js
@@ -44,3 +44,14 @@ function scrollActive(){
     }
   })
 }
+
+// ==================================================================================================
+
+
+function scrollTop(){
+    const scrollTop = document.querySelector('#scroll-top');
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll');
+    
+}
+
+window.addEventListener('scroll', scrollTop);
