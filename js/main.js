dfd30fd1796fbd7ea92ec504e8627d9439b02f22
tamimhasan19702 <tamimhasancu@gmail.com>
2022-07-09 17:11:23 +0600

    preview image

--- js/main.js
+++ js/main.js
@@ -101,7 +101,7 @@ let opt = {
     filename: 'myCv.pdf',
     image: {type: 'jpeg' , quality: 0.98 },
     html2canvas: {scale: 4},
-    jsPDF : { format: 'letter', orientation: 'portrait'}
+    jsPDF : { format: 'a4', orientation: 'portrait'}
 }
 
 
